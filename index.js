fa4ee93eecf5bbc92075735e5dd414d215feb0f8
Iko <ikotominaga@gmail.com>
2023-11-22 19:01:11 +1300

    cross browser checked for Chrome, Firefox, Safari

--- index.js
+++ index.js
@@ -9,33 +9,28 @@ const data = fetch(dataUrl)
     console.log(error)
     throw new Error ('somethingwent wrong with fetching')
 }) 
-console.log(data)
+
 const profileContainer= document.getElementById('profilContainer')
 function createProfileCard(profile){
-    const{picture, location, phone, name, login }=profile
+    const{picture, location, phone, name, login } = profile
     const card = document.createElement('div')
     card.classList.add("card")
     card.setAttribute('key', login.uuid)
     profileContainer.appendChild(card)
-    // creating inside the card div
     const image = picture.large
     const fullName = `${name.first} ${name.last}`
+
     card.innerHTML = `
-            <img src=${image}  class="card_img"/>
+            <img src=${image}  class="card_img" alt=${name}/>
             <h3>${fullName}</h3> 
             <div class="address_div">
-             <p>${location.street.number}</p>
-             <p>${location.street.name}</p>
+             <p>${location.street.number} ${location.street.name}</p>
              <p>${location.city}</p>
              <p>${location.state} ${location.postcode}</p>
              <p>${location.country}</p>
             </div>
-            <p>${phone}</p>
-     
-           
+            <p>ph: ${phone}</p>
     `
-
-  
 }
 
 
